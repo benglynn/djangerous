6d44c722383de5d8553c58c8945f1af3a13cc791
benglynn <benglynn@benglynn.net>
2010-07-31 11:01:31 +0000

    Added missing __init__.py, fixed syntax error in model.

--- djangerous/models.py
+++ djangerous/models.py
@@ -5,4 +5,4 @@ class Post(models.Model):
     Store post details returned by posterous api.
     """
     title = models.CharField(max_length=180)
-    body = models.TextField(null=True. blank=True)
+    body = models.TextField(null=True, blank=True)
